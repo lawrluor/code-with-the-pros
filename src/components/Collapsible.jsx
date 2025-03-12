6d487ad8a2c510a0fdc600cac7bf0ad09285418f
Lawrence Luo <luolawrence1@gmail.com>
2025-03-12 13:28:41 -0700

    updated collapsible style

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import { IoChevronDownSharp, IoChevronUpSharp } from "react-icons/io5";
+
 
 export default function Collapsible({ title="Challenge", children }) {
 	const [shown, setShown] = useState(false);
 
 	return (
 		<div className="collapsible">
-			<h5 className="pointer" onClick={() => setShown(!shown)}>{shown ? "Hide" : "View"} {title}</h5>
-			{shown && <div className="collapsible-content">{children}</div>}
+			<div style={{ display: "flex", alignItems: "center"}}>
+				<h5 className="pointer" onClick={() => setShown(!shown)}>{shown ? "Hide" : "View"} {title}</h5>
+				<h5 className="pointer" style={{ marginLeft: 2 }} onClick={() => setShown(!shown)}> {shown ? <IoChevronUpSharp /> : <IoChevronDownSharp />}</h5>
+			</div>
+			{shown && <div className="collapsible-content" style={{ backgroundColor: "#262626", padding: "2px 10px", margin: "10px 0px"}}>{children}</div>}
 		</div>
 	);
 }
