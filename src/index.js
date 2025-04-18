6dafaf435231999edcb3465e9cde5f7150657b58
Lawrence Luo <luolawrence1@gmail.com>
2025-04-18 14:51:59 -0700

    updated content security policy to be created dynamically

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// This function injects the Content Security Policy (CSP) into public/index.html document
+export function setContentSecurityPolicy() {
+  let connectSrc = [
+    "'self'",
+    "https://firebaseinstallations.googleapis.com",
+    "https://firebase.googleapis.com",
+    "https://www.google-analytics.com",
+    "https://codewiththepros-backend.onrender.com"
+  ];
+
+  if (process.env.NODE_ENV === 'development') {
+    // Only add localhost/127.0.0.1 during development
+    connectSrc.push("http://localhost", "http://127.0.0.1");
+  }
+
+  const cspContent = `connect-src ${connectSrc.join(' ')};`;
+
+  const cspMetaTag = document.getElementById('dynamic-csp');
+  if (cspMetaTag) {
+    cspMetaTag.setAttribute('http-equiv', 'Content-Security-Policy');
+    cspMetaTag.setAttribute('content', cspContent);
+  }
+}
+
+setContentSecurityPolicy();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
