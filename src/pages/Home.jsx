18ddb7f143308b2eb21c408dfc15872a10664843
Lawrence Luo <luolawrence1@gmail.com>
2025-12-12 23:26:34 -0800

    restyled Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,29 @@
+import ProgressBar from '../components/ProgressBar';
 import Contents from './Contents';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="lesson-container">
+      <ProgressBar />
       <section className="homeIntro">
         <h1>Code With The Pros</h1>
 
-        <p className="homeLead">
+        <p>
           A website for learning programming and coding created by Lawrence Luo. This is an interactive digital textbook.
           Through challenges throughout the reading, you will actively practice as you learn. This website is best used
           with a tutor next to you, guiding you step-by-step, asking the right questions, and helping you form strong
           habits.
         </p>
 
-        <div className="homeColumns">
-          <div className="homeColumn">
-            <h2>What To Expect</h2>
-            <p>Content is grouped into lessons/chapters. A typical chapter includes:</p>
-            <h4>Reading</h4>
-            <p>Read descriptions and code examples, one concept at a time.</p>
-            <h4>Code challenges</h4>
-            <p>Write real code after each section in small, focused exercises.</p>
-            <h4>Reasoning challenges</h4>
-            <p>Explain what code does or should do, predict outputs, and trace logic to build understanding.</p>
-            <h4>Quizzes</h4>
-            <p>Quick checks for key ideas and vocabulary.</p>
-            <h4>Projects</h4>
-            <p>Longer, creative, multi-step exercises that combine skills from previous chapters and mirror real-world tasks.</p>
-          </div>
-        </div>
+        <p>Content is grouped into lessons/chapters. A typical chapter includes:</p>
+        <ul>
+          <li><strong className="listItem">Reading:</strong> Read descriptions and code examples, one concept at a time.</li>
+          <li><strong className="listItem">Code challenges:</strong> Write real code after each section in small, focused exercises.</li>
+          <li><strong className="listItem">Reasoning challenges:</strong> Explain what code does or should do, predict outputs, and understand logic.</li>
+          <li><strong className="listItem">Quizzes:</strong> Quick checks for key ideas and vocabulary.</li>
+          <li><strong className="listItem">Projects:</strong> Longer, creative, multi-step exercises that combine skills from previous chapters and mirror real-world tasks.</li>
+        </ul>
 
         <h2>Contents</h2>
         <Contents />
